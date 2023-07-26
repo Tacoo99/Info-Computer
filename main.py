a2b02d3b3f5c2cd125d18c83ae58af68a8d2851f
Wojtek <koziolwojciech99@gmail.com>
2023-07-26 17:32:36 +0200

    New application design(scrollable frames)

--- main.py
+++ main.py
@@ -2,11 +2,41 @@ import customtkinter
 import os
 from PIL import Image
 
+
+
+class ScrollableLabelButtonFrame(customtkinter.CTkScrollableFrame):
+    def __init__(self, master, command=None, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_columnconfigure(0, weight=1)
+
+        self.command = command
+        self.radiobutton_variable = customtkinter.StringVar()
+        self.label_list = []
+        self.button_list = []
+
+    def add_item(self, item, image=None):
+        label = customtkinter.CTkLabel(self, text=item, image=image, compound="left", padx=5, anchor="w")
+        button = customtkinter.CTkButton(self, text="Command", width=100, height=24)
+        if self.command is not None:
+            button.configure(command=lambda: self.command(item))
+        label.grid(row=len(self.label_list), column=0, pady=(0, 10), sticky="w")
+        button.grid(row=len(self.button_list), column=1, pady=(0, 10), padx=5)
+        self.label_list.append(label)
+        self.button_list.append(button)
+
+    def remove_item(self, item):
+        for label, button in zip(self.label_list, self.button_list):
+            if item == label.cget("text"):
+                label.destroy()
+                button.destroy()
+                self.label_list.remove(label)
+                self.button_list.remove(button)
+                return
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        
-        self.toplevel_window = None
 
         self.title("Info-Komputer")
         self.resizable(False, False)
@@ -19,13 +49,10 @@ class App(customtkinter.CTk):
         y = (screen_height - self.winfo_height()) // 2
         self.geometry(f"+{x}+{y}")
         
-        # grid 2x2
-        self.grid_rowconfigure(1, weight=1)
-        self.grid_columnconfigure(1, weight=1)
-        
         # load images with light and dark mode image
-        lightImage_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images\light")
-        darkImage_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images\dark")
+        lightImage_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images", "light")
+        darkImage_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images", "dark")
+
         
         
         self.username_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(darkImage_path, "username_dark.png")),
@@ -47,47 +74,53 @@ class App(customtkinter.CTk):
         self.website_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(darkImage_path, "website_dark.png")),
                                                         dark_image=Image.open(os.path.join(lightImage_path, "website_light.png")), size=(30, 30))
         
-         # create home frame
-        self.home_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
-        self.home_frame.grid_columnconfigure(0, weight=2)
+        
+        computer_titles= ["Computer name","Serial Number","Windows name","Windows version","bit","Wi-Fi IP address","Ethernet IP address","CPU","GPU","RAM","BIOS"]
+        computer_images=["laptop", "serial", "windows","version","bit","wifi","ethernet","cpu","gpu","ram","bios"]
+        
+        user_titles = ["Username","Domain","E-mail","Account status","Password expiry","Servicedesk phone","Servicedesk website"]
+        user_images = ["username","domain","email","account-status","password-expiry","servicedesk", "website"]
 
-        self.banner_text = customtkinter.CTkLabel(self.home_frame, text="Info Computer",
-                                                  font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.banner_text.grid(row=0, column=0, sticky="nsew", ipady=10)
-        self.description_text = customtkinter.CTkLabel(self.home_frame, text="Some information about your computer",
-                                                  font=customtkinter.CTkFont(size=15))
-        self.description_text.grid(row=1, column=0, padx=35, sticky="n")
+        # create scrollable label and button frame
+        self.scrollable_userInfo = ScrollableLabelButtonFrame(master=self, width=300, command=self.label_button_frame_event, label_text="Computer information")
+        self.scrollable_userInfo.grid(row=0, column=1, padx=15, pady=15, sticky="nsew")
         
-        self.computername_label = customtkinter.CTkLabel(self.home_frame, text="Username", font=customtkinter.CTkFont(size=14), image=self.username_image, compound="left")
-        self.computername_label.grid(row=2, column=0, padx=0, pady=10, sticky="nsew")
-        self.username_entry = customtkinter.CTkEntry(self.home_frame, width=200, placeholder_text="Username")
-        self.username_entry.grid(row=2, column=0, padx=30, pady=(15, 15), ipady=5)
+        for i, text in enumerate(user_titles):
+            image_filename = user_images[i]
+
+            # Tworzenie ścieżek do obrazków dla obu trybów (light i dark)
+            light_image_path = os.path.join(lightImage_path, f"{image_filename}_light.png")
+            dark_image_path = os.path.join(darkImage_path, f"{image_filename}_dark.png")
+
+            # Tworzenie obiektu CTkImage
+            image = customtkinter.CTkImage(light_image=Image.open(dark_image_path), dark_image=Image.open(light_image_path), size=(30, 30))
+
+            # Dodawanie elementu do listy
+            self.scrollable_userInfo.add_item(f"{text}", image=image)
+
+        # create scrollable label and button frame
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.scrollable_computerInfo = ScrollableLabelButtonFrame(master=self, width=300, command=self.label_button_frame_event, label_text="User information")
+        self.scrollable_computerInfo.grid(row=0, column=2, padx=15, pady=15, sticky="nsew")
         
-        
-        self.username_label = customtkinter.CTkLabel(self.home_frame, text="Computer name", font=customtkinter.CTkFont(size=14), image=self.laptop_image, compound="left")
-        self.username_label.grid(row=3, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.serial_label = customtkinter.CTkLabel(self.home_frame, text="Serial Number", font=customtkinter.CTkFont(size=14), image=self.serial_image, compound="left")
-        self.serial_label.grid(row=4, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.windows_label = customtkinter.CTkLabel(self.home_frame, text="Windows version", font=customtkinter.CTkFont(size=14), image=self.windows_image, compound="left")
-        self.windows_label.grid(row=5, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.wifi_label = customtkinter.CTkLabel(self.home_frame, text="WI-FI IP Address", font=customtkinter.CTkFont(size=14), image=self.wifi_image, compound="left")
-        self.wifi_label.grid(row=6, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.ethernet_label = customtkinter.CTkLabel(self.home_frame, text="Ethernet IP Address", font=customtkinter.CTkFont(size=14), image=self.ethernet_image, compound="left")
-        self.ethernet_label.grid(row=7, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.servicedesk_label = customtkinter.CTkLabel(self.home_frame, text="Servicedesk phone", font=customtkinter.CTkFont(size=14), image=self.servicedesk_image, compound="left")
-        self.servicedesk_label.grid(row=8, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.website_label = customtkinter.CTkLabel(self.home_frame, text="Servicedesk website", font=customtkinter.CTkFont(size=14), image=self.website_image, compound="left")
-        self.website_label.grid(row=9, column=0, padx=0, pady=10, sticky="nsew")
-        
-        self.home_frame.grid(row=0, column=1)
-        
-        
+        for i, text in enumerate(computer_titles):
+            image_filename = computer_images[i]
+
+            # Tworzenie ścieżek do obrazków dla obu trybów (light i dark)
+            light_image_path = os.path.join(lightImage_path, f"{image_filename}_light.png")
+            dark_image_path = os.path.join(darkImage_path, f"{image_filename}_dark.png")
+
+            # Tworzenie obiektu CTkImage
+            image = customtkinter.CTkImage(light_image=Image.open(dark_image_path), dark_image=Image.open(light_image_path), size=(30, 30))
+
+            # Dodawanie elementu do listy
+            self.scrollable_computerInfo.add_item(f"{text}", image=image)
+
+    def label_button_frame_event(self, item):
+        print(f"label button frame clicked: {item}")
+
+
 if __name__ == "__main__":
+    customtkinter.set_appearance_mode("dark")
     app = App()
     app.mainloop()
